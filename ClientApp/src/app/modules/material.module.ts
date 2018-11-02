72447b0237797cd0ad7edb06ec867f1eeeada36e
robertka <robertka@infosupport.com>
2018-11-02 12:36:32 +0100

    More material design, bootstrap exit

--- ClientApp/src/app/modules/material.module.ts
+++ ClientApp/src/app/modules/material.module.ts
@@ -1,9 +1,23 @@
 import { NgModule } from '@angular/core';
 
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatButtonModule, MatCheckboxModule, MatIconModule} from '@angular/material';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+
 
 @NgModule({
-  imports: [MatButtonModule, MatCheckboxModule],
-  exports: [MatButtonModule, MatCheckboxModule],
+  imports: [
+    MatButtonModule, MatCheckboxModule, MatIconModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+  ],
+  exports: [
+    MatButtonModule, MatCheckboxModule, MatIconModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+  ],
 })
 export class MaterialModule { }
