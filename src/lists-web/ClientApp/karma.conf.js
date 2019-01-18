c7207f115b9931629b832af9c1bb8698b383bb5e
robertka <robert.te.kaat@gmail.com>
2019-01-18 13:50:02 +0100

    junit reporting enabled for Karma

--- src/lists-web/ClientApp/karma.conf.js
+++ src/lists-web/ClientApp/karma.conf.js
@@ -10,6 +10,7 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-phantomjs-launcher'),
       require('karma-jasmine-html-reporter'),
+      require('karma-junit-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
@@ -21,7 +22,7 @@ module.exports = function (config) {
       fixWebpackSourcePaths: true
     },
     
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml', 'junit'], // -reporters=junit
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
